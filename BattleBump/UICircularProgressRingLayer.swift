fb1a020616fec84c669288e27e1db555b7b98f2d
callosaurus <callumdavies421@gmail.com>
2017-03-06 18:33:34 -0800

    add UICircularProgressRing swift files

---
+++ BattleBump/UICircularProgressRingLayer.swift
@@ -0,0 +1,9 @@
+//
+//  UICircularProgressRingLayer.swift
+//  BattleBump
+//
+//  Created by Callum Davies on 2017-03-06.
+//  Copyright © 2017 Dave Augerinos. All rights reserved.
+//
+
+import Foundation
---
+++ BattleBump/UICircularProgressRingView.swift
@@ -0,0 +1,9 @@
+//
+//  UICircularProgressRingView.swift
+//  BattleBump
+//
+//  Created by Callum Davies on 2017-03-06.
+//  Copyright © 2017 Dave Augerinos. All rights reserved.
+//
+
+import Foundation
